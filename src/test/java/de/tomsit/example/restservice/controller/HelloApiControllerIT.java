5360bf5384767a674858d0d621c084cc29580e6c
thomas menzel <8225602+elonderin@users.noreply.github.com>
2025-11-12 16:24:50 +0100

    refactor to use WebTestClient b/c that knows the random port OOTB

--- src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
+++ src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
@@ -1,44 +1,38 @@
 package de.tomsit.example.restservice.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
-import de.tomsit.example.restservice.RestAssuredConfig;
 import de.tomsit.example.restservice.model.HelloResponse;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@Import(RestAssuredConfig.class)
 class HelloApiControllerIT {
 
+  @Autowired
+  private WebTestClient webTestClient;
 
   @Test
   void testPublicHello() {
-    HelloResponse response = RestAssured.given()
-                                        .when()
-                                        .get("/public/hello")
-                                        .then()
-                                        .statusCode(200)
-                                        .extract()
-                                        .as(HelloResponse.class);
-
-    assertThat(response).returns(HelloApiController.PUBLIC_HELLO_WORLD, HelloResponse::getMessage);
+    webTestClient.get()
+                 .uri("/public/hello")
+                 .exchange()
+                 .expectStatus().isOk()
+                 .expectBody(HelloResponse.class)
+                 .value(HelloResponse::getMessage, equalTo(HelloApiController.PUBLIC_HELLO_WORLD));
   }
 
   @Test
   void testAdminHello() {
-    HelloResponse response = RestAssured.given()
-                                        .auth().preemptive().basic("admin", "adminpass")
-                                        .when()
-                                        .get("/admin/hello")
-                                        .then()
-                                        .statusCode(200)
-                                        .extract()
-                                        .as(HelloResponse.class);
-
-    assertThat(response).returns(HelloApiController.ADMIN_HELLO_WORLD, HelloResponse::getMessage);
+    webTestClient.get()
+                 .uri("/admin/hello")
+                 .headers(h -> h.setBasicAuth("admin", "adminpass"))
+                 .exchange()
+                 .expectStatus().isOk()
+                 .expectBody(HelloResponse.class)
+                 .value(HelloResponse::getMessage, equalTo(HelloApiController.ADMIN_HELLO_WORLD));
   }
 }
