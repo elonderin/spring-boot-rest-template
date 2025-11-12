9f351c8ffc4cb6ca67f6bac52d4273fd8e67734c
thomas menzel <8225602+elonderin@users.noreply.github.com>
2025-11-12 15:31:16 +0100

    add 2 Hello EPs w/oo security

--- src/main/java/de/tomsit/example/restservice/controller/HelloApiController.java
+++ src/main/java/de/tomsit/example/restservice/controller/HelloApiController.java
@@ -8,10 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloApiController implements HelloApi {
 
+  public static final String PUBLIC_HELLO_WORLD = "public: Hello, World!";
+  public static final String ADMIN_HELLO_WORLD = "admin: Hello, World!";
+
   @Override
-  public ResponseEntity<HelloResponse> getHello() {
+  public ResponseEntity<HelloResponse> publicHello() {
+    return returnHello(PUBLIC_HELLO_WORLD);
+  }
+
+  @Override
+  public ResponseEntity<HelloResponse> adminHello() {
+    return returnHello(ADMIN_HELLO_WORLD);
+  }
+
+  private ResponseEntity<HelloResponse> returnHello(String message) {
     HelloResponse response = new HelloResponse();
-    response.setMessage("Hello, World!");
+    response.setMessage(message);
     return ResponseEntity.ok(response);
   }
 }
--- src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
+++ src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
@@ -13,19 +13,24 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HelloApiControllerIT {
 
-
   @Autowired
   private TestRestTemplate restTemplate;
 
+  @Test
+  void testPublicHello() {
+    assertThat(getHelloFrom("/public/hello"))
+        .returns(HelloApiController.PUBLIC_HELLO_WORLD, HelloResponse::getMessage);
+  }
 
   @Test
-  void getHelloWorld() {
-
-    var response = restTemplate.getForObject("/hello", HelloResponse.class, Map.of());
-
-    assertThat(response)
-        .as("spot testing some controller response")
-        .returns("Hello, World!", HelloResponse::getMessage);
+  void testAdminHello() {
+    assertThat(getHelloFrom("/admin/hello"))
+        .returns(HelloApiController.ADMIN_HELLO_WORLD, HelloResponse::getMessage);
 
   }
+
+  private HelloResponse getHelloFrom(String path) {
+    return restTemplate.getForObject(path, HelloResponse.class, Map.of());
+  }
+
 }
