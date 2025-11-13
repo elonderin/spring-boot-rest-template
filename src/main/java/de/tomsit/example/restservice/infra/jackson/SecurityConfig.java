7715c5d36e1870332d210163ae7fc02945b18195
thomas menzel <8225602+elonderin@users.noreply.github.com>
2025-11-13 11:43:30 +0100

    adds Jwt auth | with dummy decoder secret

--- src/main/java/de/tomsit/example/restservice/infra/jackson/SecurityConfig.java
+++ src/main/java/de/tomsit/example/restservice/infra/jackson/SecurityConfig.java
@@ -1,5 +1,7 @@
 package de.tomsit.example.restservice.infra.jackson;
 
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -10,41 +12,75 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@ConditionalOnWebApplication //needed so ITs w/o dont error out when running this config
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails user = User.withUsername("user")
-                .password(encoder.encode("password"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("adminpass"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    UserDetails user = User.withUsername("user")
+                           .password(encoder.encode("password"))
+                           .roles("USER")
+                           .build();
+    UserDetails admin = User.withUsername("admin")
+                            .password(encoder.encode("adminpass"))
+                            .roles("ADMIN")
+                            .build();
+    return new InMemoryUserDetailsManager(user, admin);
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(AbstractHttpConfigurer::disable) // disable for REST APIs
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/public/**").permitAll() // allow unauthenticated
-                .anyRequest().authenticated()
-            )
-            .httpBasic(); // simple username/password via HTTP Basic
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable) // disable for REST APIs
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/public/**").permitAll() // allow unauthenticated
+            .anyRequest().authenticated()
+        )
+        .httpBasic() // simple username/password via HTTP Basic
+        .and()
+        .oauth2ResourceServer(oauth ->
+                                  oauth
+                                      .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+        )
+    ;
 
-        return http.build();
-    }
+    return http.build();
+  }
+
+  private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    var converter = new JwtGrantedAuthoritiesConverter();
+    converter.setAuthoritiesClaimName("roles");    // claim to read
+    converter.setAuthorityPrefix("");         // add prefix if needed
+
+    var jwtAuthConverter = new JwtAuthenticationConverter();
+    jwtAuthConverter.setJwtGrantedAuthoritiesConverter(converter);
+    return jwtAuthConverter;
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    //bean must be defined here b/c overriding it in a test causes an IllegalStateException
+    var secret = "testsecret1234567890testsecret1234567890";
+    var key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+
+    return NimbusJwtDecoder
+        .withSecretKey(key)
+        .build();
+  }
+
+
 }
--- src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
+++ src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
@@ -3,12 +3,19 @@ package de.tomsit.example.restservice.controller;
 import static org.hamcrest.Matchers.equalTo;
 
 import de.tomsit.example.restservice.model.HelloResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import javax.crypto.spec.SecretKeySpec;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.RequestHeadersSpec;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HelloApiControllerIT {
@@ -30,33 +37,65 @@ class HelloApiControllerIT {
   @Nested
   class AdminHelloTests {
 
+    private final RequestHeadersSpec<?> adminHelloReq = webTestClient.get()
+                                                                     .uri("/admin/hello");
+
     @Test
     void withoutAuth_shouldReturnUnauthorized() {
-      webTestClient.get()
-                   .uri("/admin/hello")
-                   .exchange()
-                   .expectStatus().isUnauthorized();
+      adminHelloReq
+          .exchange()
+          .expectStatus().isUnauthorized();
     }
 
     @Test
     void withUserAuth_shouldReturnForbidden() {
-      webTestClient.get()
-                   .uri("/admin/hello")
-                   .headers(h -> h.setBasicAuth("user", "password"))
-                   .exchange()
-                   .expectStatus().isForbidden();
+      adminHelloReq
+          .headers(h -> h.setBasicAuth("user", "password"))
+          .exchange()
+          .expectStatus().isForbidden();
     }
 
     @Test
     void withAdminAuth_shouldReturnText() {
-      webTestClient.get()
-                   .uri("/admin/hello")
-                   .headers(h -> h.setBasicAuth("admin", "adminpass"))
-                   .exchange()
-                   .expectStatus().isOk()
-                   .expectBody(HelloResponse.class)
-                   .value(HelloResponse::getMessage,
-                          equalTo(HelloApiController.buildMessage("admin", "admin")));
+      adminHelloReq
+          .headers(h -> h.setBasicAuth("admin", "adminpass"))
+          .exchange()
+          .expectStatus().isOk()
+          .expectBody(HelloResponse.class)
+          .value(HelloResponse::getMessage,
+                 equalTo(HelloApiController.buildMessage("admin", "admin")));
     }
+
+    @Nested
+    class JwtIntegrationTest {
+
+      public static final String JWT_SECRET = "testsecret1234567890testsecret1234567890";
+
+
+      @Test
+      void shouldAccessEndpointWithValidJwt() {
+        Key key = new SecretKeySpec(JWT_SECRET.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+
+        var token = Jwts.builder()
+                        .setSubject("jwt-user")
+                        .claim("scope", "read")
+                        .claims().add("roles", List.of("ROLE_ADMIN", "ADMIN"))
+                        .and()
+                        .setIssuedAt(new Date())
+                        .setExpiration(new Date(System.currentTimeMillis() + 60000))
+                        .signWith(key)
+                        .compact();
+
+        adminHelloReq
+            .headers(h -> h.setBearerAuth(token))
+            .exchange()
+            .expectStatus().is2xxSuccessful()
+            .expectBody(String.class)
+            .consumeWith(result -> System.out.println(result.getResponseBody()));
+      }
+    }
+
   }
+
+
 }
