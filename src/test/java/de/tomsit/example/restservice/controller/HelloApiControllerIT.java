75e299185f958832914d7f385e8665e81de75b83
thomas menzel <8225602+elonderin@users.noreply.github.com>
2025-11-13 14:37:39 +0100

    cleanup: removes the hardcoded JwtDecoder and test

--- src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
+++ src/test/java/de/tomsit/example/restservice/controller/HelloApiControllerIT.java
@@ -3,12 +3,6 @@ package de.tomsit.example.restservice.controller;
 import static org.hamcrest.Matchers.equalTo;
 
 import de.tomsit.example.restservice.model.HelloResponse;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import java.security.Key;
-import java.util.Date;
-import java.util.List;
-import javax.crypto.spec.SecretKeySpec;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,35 +58,6 @@ class HelloApiControllerIT {
           .expectBody(HelloResponse.class)
           .value(HelloResponse::getMessage,
                  equalTo(HelloApiController.buildMessage("admin", "admin")));
-    }
-
-    @Nested
-    class JwtIntegrationTest {
-
-      public static final String JWT_SECRET = "testsecret1234567890testsecret1234567890";
-
-
-      @Test
-      void shouldAccessEndpointWithValidJwt() {
-        Key key = new SecretKeySpec(JWT_SECRET.getBytes(), SignatureAlgorithm.HS256.getJcaName());
-
-        var token = Jwts.builder()
-                        .setSubject("jwt-user")
-                        .claim("scope", "read")
-                        .claims().add("roles", List.of("ROLE_ADMIN", "ADMIN"))
-                        .and()
-                        .setIssuedAt(new Date())
-                        .setExpiration(new Date(System.currentTimeMillis() + 60000))
-                        .signWith(key)
-                        .compact();
-
-        adminHelloReq
-            .headers(h -> h.setBearerAuth(token))
-            .exchange()
-            .expectStatus().is2xxSuccessful()
-            .expectBody(String.class)
-            .consumeWith(result -> System.out.println(result.getResponseBody()));
-      }
     }
 
   }
